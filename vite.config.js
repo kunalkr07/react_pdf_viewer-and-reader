971d60b4145640cd8396d81c929cef01f0a5149e
kunalWebDev07 <168664400+kunalkr07@users.noreply.github.com>
2024-12-19 22:49:35 +0530

    files removed

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,11 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ['@fortawesome/free-solid-svg-icons'],
+      external: ["@fortawesome/free-solid-svg-icons"],
     },
   },
-  
 });
