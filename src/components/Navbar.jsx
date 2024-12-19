b77ada2bb1b819b6e1de1aa7bb972610c0fad9ac
kunalWebDev07 <168664400+kunalkr07@users.noreply.github.com>
2024-12-20 03:37:17 +0530

    img container from about section removed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,10 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
-import 'animate.css'; 
+import "animate.css";
 
 export default function Navbar({ onSearch }) {
   const [input, setInput] = useState("");
   const [isOpen, setOpen] = useState(false);
-  
 
   // Navbar scroll function
   window.addEventListener("scroll", () => {
@@ -35,37 +34,43 @@ export default function Navbar({ onSearch }) {
   //toggle sidebar
   const toggleSidebar = () => {
     console.log("toggled side bar");
-   
-      setOpen((prev) => !prev);
-   
-   
+
+    setOpen((prev) => !prev);
   };
 
   //close sidebar when click on mark
-  const closeTab=()=>{
-    setOpen(false)
-  }
+  const closeTab = () => {
+    setOpen(false);
+  };
 
   return (
     <div>
       <nav>
         <div className="logo-name">
-        <i
-          className="fa-duotone fa-solid fa-hexagon-nodes"
-          onClick={toggleSidebar}
-        ></i>
-          
+          <i
+            className="fa-duotone fa-solid fa-hexagon-nodes"
+            onClick={toggleSidebar}
+          ></i>
         </div>
 
-        <ul className={`links ${isOpen ? "active" : ""} animate__animated animate__backInDown`}>
-
-        <i className="fa-solid fa-x mark" onClick={closeTab}></i>
+        <ul
+          className={`links ${
+            isOpen ? "active" : ""
+          } animate__animated animate__backInDown`}
+        >
+          <i className="fa-solid fa-x mark" onClick={closeTab}></i>
           <li>
             <Link to="/about">About</Link>
           </li>
-          <h5>It is a simple and efficient tool designed to streamline document management. With this project, <br /> you can create, organize, and download PDF files with ease. <br />Whether you're looking to compile your resume, manage reports, or store essential files,<br /> PDFList offers a user-friendly interface and seamless functionality to meet your needs.</h5>
-         
-  
+          <h5>
+            It is a simple and efficient tool designed to streamline document
+            management. With this project, <br /> you can create, organize, and
+            download PDF files with ease. <br />
+            Whether you're looking to compile your resume, manage reports, or
+            store essential files,
+            <br /> PDFList offers a user-friendly interface and seamless
+            functionality to meet your needs.
+          </h5>
         </ul>
 
         {/* serach bar starts */}
