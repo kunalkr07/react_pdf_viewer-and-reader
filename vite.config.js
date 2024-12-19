9bf92aed9c50b99df25d4f2f43c63c9ec3aa6d5a
kunalWebDev07 <168664400+kunalkr07@users.noreply.github.com>
2024-12-19 23:12:18 +0530

    vite changes

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,10 @@ export default defineConfig({
   plugins: [react()],
   build: {
     rollupOptions: {
-      external: ["@fortawesome/free-solid-svg-icons"],
+      external: [
+        "@fortawesome/free-solid-svg-icons", 
+        "react-router-dom" // Add react-router-dom here
+      ],
     },
   },
 });
